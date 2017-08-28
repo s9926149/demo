cb084d401a082fe917ad8ea03cd25b82a25d5599
Ellis <ellis@eztravel.com.tw>
2017-08-28 17:52:59 +0800

    Building a REST service

---
+++ src/main/java/com/example/demo/bookmarks/BookmarkRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.bookmarks;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+  Collection<Bookmark> findByAccountUsername(String username);
+}
